b2998cda042369d633771cd465c15c5f961802d7
Kenny <kennyvungo@gmail.com>
2024-02-27 23:41:03 -0800

    fixed types for projectlink function, started work layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,8 @@ import { FaLinkedin, FaThumbsDown } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaRegFilePdf } from "react-icons/fa";
 import { GrDocumentText } from "react-icons/gr";
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
 
-
-function ProjectLink({ slug, name, sub, stack, thumb }) {
+function ProjectLink({ slug, name, sub, stack, thumb }:{slug:string, name:string, sub:string, stack:string, thumb:string}) {
   return (
     <div>
       <a
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import Link from 'next/link';
 
 const WorkPage = () => {
   return (
-    <div>WorkPage</div>
-  )
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          I've worked in many settings 
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">BottleUp</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Software Engineer Intern, Oct 2023 - Present
+        </p>
+        <p>
+            adfadfadfadffasdfaf
+        </p>
+
+        <p>adfadfadf</p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Freelance</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Software Engineer, Jul 2023 — Present
+        </p>
+        <p>H</p>
+
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Alto</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Pharmacist
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+          Castle Family Health Center
+        </h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Pharmacy Manager
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+          BJRX Long Term Care
+        </h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Pharmacist
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+          CVS Health
+        </h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Pharmacist
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default WorkPage
